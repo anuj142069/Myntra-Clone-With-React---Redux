f190ff15f8f009a3c82032eeaf59c749cb370eae
anuj142069 <anujkumar14102002@gmail.com>
2026-03-05 19:04:12 +0530

    Update FetchItems.jsx

--- myntra-react-clone/src/components/FetchItems.jsx
+++ myntra-react-clone/src/components/FetchItems.jsx
@@ -13,7 +13,7 @@ const FetchItems = () => {
     const controller = new AbortController();
     const signal = controller.signal;
     // dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("https://myntra-clone-with-react-redux-and-node.onrender.com", {signal})
+    fetch("https://myntra-clone-with-react-redux-and-node.onrender.com/items", {signal})
     .then((res) => res.json())
     .then(({items}) => {
       // dispatch(fetchStatusActions.markFetchDone());
